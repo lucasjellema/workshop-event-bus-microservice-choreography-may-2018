575d5de17fd3c2266030c3d17c73caed39461f63
Lucas Jellema <Lucas.Jellema@amis.nl>
2018-04-20 06:46:35 +0200

    Alternative Consumer registration

--- part3/event-bus-listener/EventBusListener.js
+++ part3/event-bus-listener/EventBusListener.js
@@ -6,17 +6,16 @@ var http = require('http'),
 
 var kafkaHost = process.env.KAFKA_HOST || "ubuntu";
 var zookeeperPort = process.env.ZOOKEEPER_PORT || 2181;
-//var kafkaHostIP = "192.168.188.101";
+
 var Consumer = kafka.Consumer
 var Producer = kafka.Producer
 KeyedMessage = kafka.KeyedMessage;
 
-//var kafkaTopic = process.env.KAFKA_TOPIC || "event-bus";
 var kafkaTopic = process.env.KAFKA_TOPIC || "workflowEvents";
 var client;
 var consumer;
 var PORT = process.env.APP_PORT || 8096;
-var APP_VERSION = "0.8"
+var APP_VERSION = "0.8.3"
 var APP_NAME = "EventBusListener"
 
 
@@ -57,10 +56,46 @@ app.get('/event-bus', function (req, res) {
 
 var events = [];
 
+var consumerOptions = {
+  host: kafkaHost + ":"+zookeeperPort,
+  groupId: 'event-bus-listener',
+  sessionTimeout: 15000,
+  protocol: ['roundrobin'],
+  fromOffset: 'earliest' // equivalent of auto.offset.reset valid values are 'none', 'latest', 'earliest'
+};
+
+function onMessage(message) {
+  console.log('%s read msg Topic="%s" Partition=%s Offset=%d'
+  , this.client.clientId, message.topic, message.partition, message.offset);
+  handleEventBusMessage(message);
+
+}
+
+function onError(error) {
+  console.error(error);
+  console.error(error.stack);
+}
+
+process.once('SIGINT', function () {
+  async.each([consumerGroup], function (consumer, callback) {
+      consumer.close(true, callback);
+  });
+});
+
 
 function initializeKafkaConsumer(attempt) {
+  var topics = [kafkaTopic];
+  var consumerGroup = new kafka.ConsumerGroup(Object.assign({ id: 'consumerLocal' }, consumerOptions), topics);
+  consumerGroup.on('error', onError);
+  consumerGroup.on('message', onMessage);
+  
+  consumerGroup.on('connect', function () {
+      console.log('connected to ' + topicName + " at " + consumerOptions.host);
+  })
+  
+/*
   try {
-    console.log("Try to initialize Kafka Client and Consumer, attempt " + attempt);
+    console.log("Try to initialize Kafka Client and Consumer, attempt " + attempt+" at "+kafkaHost + ":"+zookeeperPort );
     var client = new kafka.Client(kafkaHost + ":"+zookeeperPort+"/")
     console.log("created client for " + kafkaHost);
     consumer = new Consumer(
@@ -71,7 +106,7 @@ function initializeKafkaConsumer(attempt) {
     console.log("Kafka Client and Consumer initialized " + consumer);
     // register the handler for any messages received by the consumer on any topic it is listening to. 
     consumer.on('message', function (message) {
-      console.log("event received");
+      console.log("event received "+message);
       handleEventBusMessage(message);
     });
     consumer.on('error', function (err) {
@@ -82,7 +117,7 @@ function initializeKafkaConsumer(attempt) {
     consumer.addTopics([
       { topic: kafkaTopic, partition: 0, offset: 0 }
     ], () => console.log("topic added: " + kafkaTopic));
-    console.log("Kafka Consumer - added message handler and added topic");
+    console.log("Kafka Consumer - added message handler and added topic "+kafkaTopic);
   }
   catch (err) {
     console.log("Exception in initializeKafkaConsumer" + e);
@@ -90,6 +125,7 @@ function initializeKafkaConsumer(attempt) {
     console.log("Try again in 5 seconds");
     setTimeout(initializeKafkaConsumer, 5000, attempt + 1);
   }
+  */
 }//initializeKafkaConsumer
 
 initializeKafkaConsumer(1);
