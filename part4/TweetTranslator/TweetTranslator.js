1777f0fae9424f33517ca0d83966a9f8cbb263f9
Lucas Jellema <Lucas.Jellema@amis.nl>
2018-04-21 12:21:18 +0200

    TweetTranslator - initial creation

---
+++ part4/TweetTranslator/TweetTranslator.js
@@ -0,0 +1,192 @@
+var http = require('http'),
+  request = require('request'),
+  express = require('express'),
+  bodyParser = require('body-parser')
+translateGoogle = require('google-translate-api');
+;
+
+//var localCacheAPI = require("./local-cache-api.js");
+var localLoggerAPI = require("./local-logger-api.js");
+var eventBusPublisher = require("./EventPublisher.js");
+var eventBusConsumer = require("./EventConsumer.js");
+
+var workflowEventsTopic = "workflowEvents";
+var PORT = process.env.APP_PORT || 8099;
+var APP_VERSION = "0.1.2"
+var APP_NAME = "TweetTranslator"
+
+var TweetTranslatorActionType = "TranslateTweet";
+
+console.log("Running " + APP_NAME + " version " + APP_VERSION);
+
+var app = express();
+var server = http.createServer(app);
+server.listen(PORT, function () {
+  console.log('Microservice ' + APP_NAME + 'running, Express is listening... at ' + PORT + " for /ping, /about and /tweet translation API calls");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: '*/*' }));
+app.get('/about', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("About TweetTranslator API, Version " + APP_VERSION);
+  res.write("Supported URLs:");
+  res.write("/ping (GET)\n;");
+  res.write("/tweet (POST)");
+  res.write("NodeJS runtime version " + process.version);
+  res.write("incoming headers" + JSON.stringify(req.headers));
+  res.end();
+});
+
+app.get('/ping', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("Reply from " + APP_NAME);
+  res.write("incoming headers" + JSON.stringify(req.headers));
+  res.end();
+});
+
+app.post('/tweet', function (req, res) {
+  return new Promise(function (resolve, reject) {
+    // Get the key and value
+
+    console.log('TweetTranslator - translate tweet');
+    console.log('body in request' + JSON.stringify(req.body));
+    console.log("content type " + req.headers['content-type']);
+    var tweet = req.body;
+    translate(tweet).then((translatedTweet) => {
+      var responseBody = { "translatedTweet": translatedTweet };
+      // Send the response
+      res.setHeader('Content-Type', 'application/json');
+      res.send(responseBody);
+    })
+    resolve();
+  })// Promise
+});
+
+function translate(translatingTweet) {
+
+  return new Promise((resolve, reject) => {
+    translatingTweet.translations = [];
+    Promise.all([
+      translateGoogle(translatingTweet.text, { from: "en",to: 'de' })
+      , translateGoogle(translatingTweet.text, { from: "en",to: 'es' })
+      , translateGoogle(translatingTweet.text, { from: "en",to: 'fr' })
+      , translateGoogle(translatingTweet.text, { from: "en", to: 'nl' })
+    ]).then((translations) => {
+      localLoggerAPI.log("translate  - all translations are in :" + JSON.stringify(translations) + ")"
+        , APP_NAME, "info");
+
+      translations.forEach((val) => {
+        translatingTweet.translations.push(val.text);
+      });
+      localLoggerAPI.log("translate - resolve :)"
+        , APP_NAME, "info");
+      resolve(translatingTweet);
+    }) //then
+  })//promise
+}//translate
+
+// configure Kafka interaction
+eventBusConsumer.registerEventHandler(workflowEventsTopic, handleWorkflowEvent);
+
+
+function handleWorkflowEvent(eventMessage) {
+  var event = JSON.parse(eventMessage.value);
+  console.log("received message", eventMessage);
+  console.log("actual event: " + JSON.stringify(event));
+  // event we expect is of type workflowEvents
+  // we should do something with this event if it contains an action (actions[].type=TweetTranslatorActionType where status ="new" and conditions are satisfied)
+  try {
+    if (event.actions) {
+      var acted = false;
+      for (i = 0; i < event.actions.length; i++) {
+        var action = event.actions[i];
+        // find action of type TranslateTweet
+        if (TweetTranslatorActionType == action.type) {
+          // check conditions
+          if ("new" == action.status
+            && conditionsSatisfied(action, event.actions)) {
+            var currentAction = action;
+              localLoggerAPI.log("handleWorkflowEvent : "
+              , APP_NAME, "info");
+            var workflowDocument;
+            // localCacheAPI.getFromCache(event.workflowConversationIdentifier, function (document) {
+            //   console.log("Workflow document retrieved from cache");
+            //   var workflowDocument = document;
+            //   // this happens  asynchronously; right now we do not actually use the retrieved document. It does work.       
+            // });
+
+            translate(event.payload).then((translatedTweet) => {
+
+              event.payload = translatedTweet;
+              // update action in event
+              currentAction.status = 'complete';
+              currentAction.result = 'OK';
+              // add audit line
+
+              event.audit.push(
+                { "when": new Date().getTime(), "who": "TweetTranslator", "what": "update", "comment": "Tweet Translation Performed" }
+              );
+              acted = true;
+              if (acted) {
+                event.updateTimeStamp = new Date().getTime();
+                event.lastUpdater = APP_NAME;
+
+                localLoggerAPI.log("Translated Tweet  - (workflowConversationIdentifier:" + event.workflowConversationIdentifier + ")"
+                  , APP_NAME, "info");
+
+                // PUT Workflow Document back  in Cache under workflow event identifier
+                // localCacheAPI.putInCache(event.workflowConversationIdentifier, event,
+                //   function (result) {
+                //     console.log("store workflowevent plus routing slip in cache under key " + event.workflowConversationIdentifier + ": " + JSON.stringify(result));
+                //   });
+                // publish event
+                setTimeout(() => {
+                  console.log("******** GO PUB WORKFLOW EVENT");
+                  eventBusPublisher.publishEvent('OracleCodeTwitterWorkflow' + event.updateTimeStamp, event, workflowEventsTopic);
+              }
+                  , 1500
+                );
+
+              }// acted
+            })// then
+          }
+        }// if TranslateTweet
+        // if any action performed, then republish workflow event and store routingslip in cache
+      }//for
+    }// if actions
+  } catch (err) {
+    localLoggerAPI.log("handleWorkflowEvent : EXCEPTION " + err
+      , APP_NAME, "info");
+
+  }
+}// handleWorkflowEvent
+
+function conditionsSatisfied(action, actions) {
+  var satisfied = true;
+  // verify if conditions in action are methodName(params) {
+  //   example action: {
+  //   "id": "CaptureToTweetBoard"
+  // , "type": "TweetBoardCapture"
+  // , "status": "new"  // new, inprogress, complete, failed
+  // , "result": "" // for example OK, 0, 42, true
+  // , "conditions": [{ "action": "EnrichTweetWithDetails", "status": "complete", "result": "OK" }]
+  for (i = 0; i < action.conditions.length; i++) {
+    var condition = action.conditions[i];
+    if (!actionWithIdHasStatusAndResult(actions, condition.action, condition.status, condition.result)) {
+      satisfied = false;
+      break;
+    }
+  }//for
+  return satisfied;
+}//conditionsSatisfied
+
+function actionWithIdHasStatusAndResult(actions, id, status, result) {
+  for (i = 0; i < actions.length; i++) {
+    if (actions[i].id == id && actions[i].status == status && actions[i].result == result)
+      return true;
+  }//for
+  return false;
+}//actionWithIdHasStatusAndResult
+
+setTimeout(() => { localLoggerAPI.log("Running " + APP_NAME + " version " + APP_VERSION, APP_NAME, "info") }, 2500);
---
+++ part4/TweetTranslator/local-cache-api.js
@@ -0,0 +1,105 @@
+var request = require('request')
+    ;
+    var http = require("http");
+
+//this module talks to a locally running Redis Cache
+
+var localCacheAPI = module.exports;
+var moduleName = "accs.localCacheAPI";
+var moduleVersion = "0.8.7";
+var Redis = require("redis");
+
+var redisHost = process.env.REDIS_HOST || "192.168.99.111";
+var redisPort = process.env.REDIS_PORT || 38789;
+
+var cacheInspectorServiceHost = "192.168.99.100";
+var cacheInspectorServicePort = "32507";
+
+var callViaAPI = true;
+
+var redisClient;
+
+if (!callViaAPI) {try { redisClient = Redis.createClient({ "host": redisHost, "port": redisPort })}catch (err){console.log("Exception in Redis Client "+err)}};
+
+var cacheAPIOptions = {
+    "method": "GET",
+    "hostname": cacheInspectorServiceHost,
+    "port": cacheInspectorServicePort,
+    "path": "/cacheEntry",
+    "headers": {
+        "cache-control": "no-cache",
+    }
+};
+localCacheAPI.getFromCache = function (key, callback) {
+    if (callViaAPI) {
+        cacheAPIOptions.path = "/cacheEntry?key=" + key;
+        cacheAPIOptions.method = "GET";
+        var req = http.request(cacheAPIOptions, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+                console.log(body.toString());
+                callback(JSON.parse(body));
+            });
+        });
+
+        req.end();
+    } else {
+        try {
+            console.log("get document from cache api with key " + key);
+            redisClient.get(key, function (err, reply) {
+                if (err) {
+                    console.error('ERROR in getting document from cache ' + err);
+                    callback(null);
+                } else {
+                    callback(JSON.parse(reply));
+                }//else
+            });//get
+        } catch (e) {
+            console.error('ERROR i  n accessing redis ' + e);
+            callback(null);
+        }
+    }
+}//getFromCache
+
+localCacheAPI.putInCache = function (key, value, callback) {
+    if (callViaAPI) {
+        cacheAPIOptions.path = "/cacheEntry";
+        cacheAPIOptions.method = "POST";
+        var req = http.request(cacheAPIOptions, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+
+            res.on("end", function () {
+                var body = Buffer.concat(chunks);
+                console.log(body.toString());
+            });
+        });
+
+        req.write(JSON.stringify({
+            key: key,
+            document: value
+        }));
+        req.end();
+    }
+    else {
+        try {
+            console.log("putInCache Callback = " + callback);
+            redisClient.set(key, JSON.stringify(value));
+            callback("Put in cache");
+        } catch (e) {
+            callback("Failed to put in cache " + JSON.stringify(e));
+        }
+    }
+}//putInCache
+
+
+console.log("Local Cache API (version " + moduleVersion + ") initialized running against Redis instance at " + redisHost + ":" + redisPort);
