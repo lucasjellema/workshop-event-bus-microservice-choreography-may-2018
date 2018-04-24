861d27f4953b3ef28621d2d6f1112ee6f2dbd44f
Lucas Jellema <Lucas.Jellema@amis.nl>
2018-04-24 22:45:23 +0200

    Use workflow document from cache

--- part4/ValidateTweet/TweetValidatorV2.js
+++ part4/ValidateTweet/TweetValidatorV2.js
@@ -90,7 +90,6 @@ function validateTweet(tweet) {
 // configure Kafka interaction
 eventBusConsumer.registerEventHandler(workflowEventsTopic, handleWorkflowEvent);
 
-
 function handleWorkflowEvent(eventMessage) {
   var event = JSON.parse(eventMessage.value);
   console.log("received message", eventMessage);
@@ -100,55 +99,56 @@ function handleWorkflowEvent(eventMessage) {
   // event we expect is of type workflowEvents
   // we should do something with this event if it contains an action (actions[].type='ValidateTweet' where status ="new" and conditions are satisfied)
 
-  if (event.actions) {
-    var acted = false;
-    for (i = 0; i < event.actions.length; i++) {
-      var action = event.actions[i];
-      // find action of type ValidateTweet
-      if ("ValidateTweet" == action.type) {
-        // check conditions
-        if ("new" == action.status
-          && conditionsSatisfied(action, event.actions)) {
-          var workflowDocument;
-          localCacheAPI.getFromCache(event.workflowConversationIdentifier, function (document) {
-            console.log("Workflow document retrieved from cache");
-            var workflowDocument = document;
-            // this happens  asynchronously; right now we do not actually use the retrieved document. It does work.       
+  if (containsAction(event))
+    localCacheAPI.getFromCache(event.workflowConversationIdentifier, function (document) {
+      console.log("Workflow document retrieved from cache");
+      workflowDocument = document;
+      // this happens  asynchronously; right now we do not actually use the retrieved document. It does work.       
+      var acted = false;
+      for (i = 0; i < workflowDocument.actions.length; i++) {
+        var action = workflowDocument.actions[i];
+        // find action of type ValidateTweet
+        if ("ValidateTweet" == action.type) {
+          // check conditions
+          if ("new" == action.status
+            && conditionsSatisfied(action, workflowDocument.actions)) {
+
+            // if satisfied, then validate tweet
+            var outcome = validateTweet(workflowDocument.payload);
+
+            // update action in event
+            action.status = 'complete';
+            action.result = outcome.result;
+            // add audit line
+            workflowDocument.audit.push(
+              { "when": new Date().getTime(), "who": "TweetValidator", "what": "update", "comment": "Tweet Validation Complete" }
+            );
+
+            acted = true;
+            localLoggerAPI.log("Validated Tweet (outcome:" + JSON.stringify(outcome) + ")"
+              + " - (workflowConversationIdentifier:" + workflowDocument.workflowConversationIdentifier + ")"
+              , APP_NAME, "info");
+
+          }
+        }// if ValidateTweet
+        // if any action performed, then republish workflow event and store routingslip in cache
+      }//for
+      if (acted) {
+        workflowDocument.updateTimeStamp = new Date().getTime();
+        workflowDocument.lastUpdater = APP_NAME;
+        // publish event
+        eventBusPublisher.publishEvent('OracleCodeTwitterWorkflow' + workflowDocument.updateTimeStamp, workflowDocument, workflowEventsTopic);
+
+        // PUT Workflow Document back  in Cache under workflow event identifier
+        localCacheAPI.putInCache(event.workflowConversationIdentifier, workflowDocument,
+          function (result) {
+            console.log("store workflowevent plus routing slip in cache under key " + event.workflowConversationIdentifier + ": " + JSON.stringify(result));
           });
-          // if satisfied, then validate tweet
-          var outcome = validateTweet(event.payload);
-
-          // update action in event
-          action.status = 'complete';
-          action.result = outcome.result;
-          // add audit line
-          event.audit.push(
-            { "when": new Date().getTime(), "who": "TweetValidator", "what": "update", "comment": "Tweet Validation Complete" }
-          );
-
-          acted = true;
-          localLoggerAPI.log("Validated Tweet (outcome:" + JSON.stringify(outcome) + ")"
-            + " - (workflowConversationIdentifier:" + event.workflowConversationIdentifier + ")"
-            , APP_NAME, "info");
-
-        }
-      }// if ValidateTweet
-      // if any action performed, then republish workflow event and store routingslip in cache
-    }//for
-    if (acted) {
-      event.updateTimeStamp = new Date().getTime();
-      event.lastUpdater = APP_NAME;
-      // publish event
-      eventBusPublisher.publishEvent('OracleCodeTwitterWorkflow' + event.updateTimeStamp, event, workflowEventsTopic);
-
-      // PUT Workflow Document back  in Cache under workflow event identifier
-      localCacheAPI.putInCache(event.workflowConversationIdentifier, event,
-        function (result) {
-          console.log("store workflowevent plus routing slip in cache under key " + event.workflowConversationIdentifier + ": " + JSON.stringify(result));
-        });
-    }// acted
-  }// if actions
+      }// acted
+    })
+  // if contains actions
 }// handleWorkflowEvent
+
 
 function conditionsSatisfied(action, actions) {
   var satisfied = true;
