285c6e2292c8145cdbc4de7bd74252d753afb58a
Lucas Jellema <Lucas.Jellema@amis.nl>
2018-04-24 21:24:57 +0200

    Adding locking to putInCache

--- part4/ValidateTweet/local-cache-api.js
+++ part4/ValidateTweet/local-cache-api.js
@@ -6,14 +6,14 @@ var request = require('request')
 
 var localCacheAPI = module.exports;
 var moduleName = "accs.localCacheAPI";
-var moduleVersion = "0.8.7";
+var moduleVersion = "0.9.0";
 var Redis = require("redis");
-
+var RedisLock = require("redis-lock");
 var redisHost = process.env.REDIS_HOST || "192.168.99.100";
 var redisPort = process.env.REDIS_PORT || 32657;
 
 var redisClient = Redis.createClient({ "host": redisHost, "port": redisPort });
-
+var redisLock = RedisLock(redisClient);
 localCacheAPI.getFromCache = function (key, callback) {
     try {
         console.log("get document from cache api with key " + key);
@@ -32,14 +32,21 @@ localCacheAPI.getFromCache = function (key, callback) {
 }//getFromCache
 
 localCacheAPI.putInCache = function (key, value, callback) {
+    var lockName = "myLock" + key
     try {
-        console.log("putInCache Callback = " + callback);
-        redisClient.set(key, JSON.stringify(value));
-        callback("Put in cache");
+        lock(lockName, function (done) {
+            console.log("Lock acquired");
+            console.log("putInCache Callback = " + callback);
+            redisClient.set(key, JSON.stringify(value));
+            callback("Put in cache");
+            done(function () {
+                console.log(`Lock ${lockName} has been released, and is available for others to use`);
+            });
+        });
     } catch (e) {
         callback("Failed to put in cache " + JSON.stringify(e));
     }
 }//putInCache
 
 
-console.log("Local Cache API (version " + moduleVersion + ") initialized running against Redis instance at "+redisHost+":"+redisPort);
+console.log("Local Cache API (version " + moduleVersion + ") initialized running against Redis instance at " + redisHost + ":" + redisPort);
